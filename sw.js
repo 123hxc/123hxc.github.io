2c0091413c35594cef699d258deb00afbd3e8d44
hxc <2274921053@qq.com>
2026-02-14 08:01:44 +0800

    Site updated: 2026-02-14 08:01:43

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1766947576738";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1771027296392";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
